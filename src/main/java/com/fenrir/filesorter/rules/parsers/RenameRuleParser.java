c4f793b18010fef451338936475a6fd8cc3e348e
Fenrir7734 <hitex1999@gmail.com>
2021-10-26 15:36:00 +0200

    Rename rule parser

--- src/main/java/com/fenrir/filesorter/rules/parsers/RenameRuleParser.java
+++ src/main/java/com/fenrir/filesorter/rules/parsers/RenameRuleParser.java
@@ -3,6 +3,11 @@ package com.fenrir.filesorter.rules.parsers;
 import com.fenrir.filesorter.file.FileData;
 import com.fenrir.filesorter.rules.RenameRule;
 import com.fenrir.filesorter.rules.Rule;
+import com.fenrir.filesorter.tokens.DateTokenType;
+import com.fenrir.filesorter.tokens.RenameTokenType;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class RenameRuleParser {
     private Rule rule;
@@ -11,5 +16,53 @@ public class RenameRuleParser {
     public RenameRuleParser(RenameRule rule, FileData fileData) {
         this.rule = rule;
         this.fileData = fileData;
+    }
+
+    public String resolveRule() {
+        StringBuilder resolvedRule = new StringBuilder();
+
+        Rule.RuleElement element;
+        while ((element = rule.next()) != null) {
+            if (element.isFlag()) {
+                String resolvedToken = parseToken(element.element());
+                resolvedRule.append(resolvedToken);
+            } else {
+                resolvedRule.append(element.element());
+            }
+        }
+        return null;
+    }
+
+    private String parseToken(String token) throws IllegalArgumentException {
+        DateTokenType dateTokenType = DateTokenType.get(token);
+
+        if (dateTokenType != null) {
+            return getDate(dateTokenType.getPattern());
+        }
+
+        RenameTokenType renameTokenType = RenameTokenType.get(token);
+
+        if (renameTokenType == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return switch (renameTokenType) {
+            case CURRENT_FILE_NAME -> getFileName();
+            case FILE_EXTENSION -> getFileExtension();
+        };
+    }
+
+    private String getFileExtension() {
+        String extension = fileData.getFileExtension();
+        return extension != null && !extension.equals("") ? extension : "Other";
+    }
+
+    private String getFileName() {
+        return null;
+    }
+
+    private String getDate(String pattern) {
+        Calendar calendar = fileData.creationTime();
+        return new SimpleDateFormat(pattern).format(calendar);
     }
 }
