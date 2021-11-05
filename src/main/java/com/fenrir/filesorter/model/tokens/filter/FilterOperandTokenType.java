950c3689502233795676f4f1c465511ef7e29812
Fenrir7734 <hitex1999@gmail.com>
2021-11-05 14:15:18 +0100

    token

--- src/main/java/com/fenrir/filesorter/model/tokens/filter/FilterOperandTokenType.java
+++ src/main/java/com/fenrir/filesorter/model/tokens/filter/FilterOperandTokenType.java
@@ -1,2 +1,22 @@
-package com.fenrir.filesorter.model.tokens.filter;public class FilterOperandTokenType {
+package com.fenrir.filesorter.model.tokens.filter;
+
+public enum FilterOperandTokenType {
+    CURRENT_FILE_NAME("CUR"), PATH("PAT"), DATE("DAT"), RESOLUTION("RES"), SIZE("SIZ"), FILE_EXTENSION("EXT"),
+    FILE_CATEGORY("CAT");
+
+    private final String token;
+
+    FilterOperandTokenType(String token) {
+        this.token = token;
+    }
+
+    public static FilterOperandTokenType get(String token) {
+        FilterOperandTokenType[] tokenTypes = FilterOperandTokenType.values();
+        for (FilterOperandTokenType tokenType: tokenTypes) {
+            if (tokenType.token.equals(token)) {
+                return tokenType;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/fenrir/filesorter/model/tokens/filter/FilterOperatorTokenType.java
+++ src/main/java/com/fenrir/filesorter/model/tokens/filter/FilterOperatorTokenType.java
@@ -1,2 +1,25 @@
-package com.fenrir.filesorter.model.tokens.filter;public class FilterOperatorTokenType {
+package com.fenrir.filesorter.model.tokens.filter;
+
+public enum FilterOperatorTokenType {
+    EQUAL("=="), NOT_EQUAL("!="),
+    GREATER(">"), GREATER_EQUAL(">="), SMALLER("<"), SMALLER_EQUAL("<="),
+    //NOT("!"),
+
+    CONTAINS("CON"), STARTS_WITH("SW"), ENDS_WITH("EW");
+
+    private final String token;
+
+    FilterOperatorTokenType(String token) {
+        this.token = token;
+    }
+
+    public static FilterOperatorTokenType get(String token) {
+        FilterOperatorTokenType[] tokenTypes = FilterOperatorTokenType.values();
+        for (FilterOperatorTokenType tokenType: tokenTypes) {
+            if (tokenType.token.equals(token)) {
+                return tokenType;
+            }
+        }
+        return null;
+    }
 }
