b2c141595c9734d326e6fb30832131eaf044ee15
Fenrir7734 <hitex1999@gmail.com>
2021-11-11 16:58:12 +0100

    exception

--- src/main/java/com/fenrir/filesorter/model/exceptions/RuleFormatException.java
+++ src/main/java/com/fenrir/filesorter/model/exceptions/RuleFormatException.java
@@ -1,4 +1,34 @@
-package com.fenrir.filesorter.model.expections;
+package com.fenrir.filesorter.model.exceptions;
 
-public class RuleFormatExpection {
+import com.fenrir.filesorter.model.rules.Rule;
+
+public class RuleFormatException extends Exception {
+    private Rule rule;
+
+    public RuleFormatException(String message) {
+        super(message);
+    }
+
+    public RuleFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RuleFormatException(String message, Throwable cause, Rule rule) {
+        super(message, cause);
+        this.rule = rule;
+    }
+
+    public RuleFormatException(String message, Rule rule) {
+        super(message);
+        this.rule = rule;
+    }
+
+    public RuleFormatException(Throwable cause, Rule rule) {
+        super(cause);
+        this.rule = rule;
+    }
+
+    public Rule getRule() {
+        return rule;
+    }
 }
