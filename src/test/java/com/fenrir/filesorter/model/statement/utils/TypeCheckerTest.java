4c511b35e0c4ed5a3d35ed49705853de489a4592
Fenrir7734 <hitex1999@gmail.com>
2021-11-20 22:35:46 +0100

    test

--- src/test/java/com/fenrir/filesorter/model/statement/utils/TypeCheckerTest.java
+++ src/test/java/com/fenrir/filesorter/model/statement/utils/TypeCheckerTest.java
@@ -1,6 +1,30 @@
 package com.fenrir.filesorter.model.statement.utils;
 
+import com.fenrir.filesorter.model.statement.predicate.PredicateOperands;
+import com.fenrir.filesorter.model.statement.provider.FileNameProvider;
+import com.fenrir.filesorter.model.statement.provider.FileSizeProvider;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TypeCheckerTest {
-  
+    @Test
+    public void isInstanceOfStringShouldReturnTrueIfGivenArgumentIsOfTypeString() {
+        PredicateOperands<String> operands = new PredicateOperands<>(
+                new FileNameProvider(null),
+                List.of("a", "b", "c")
+        );
+        assertTrue(TypeChecker.isInstanceOfString(operands));
+    }
+
+    @Test
+    public void isInstanceOfStringShouldReturnFalseIfGivenArgumentIsNotOfTypeString() {
+        PredicateOperands<Long> operands = new PredicateOperands<>(
+                new FileSizeProvider(null),
+                List.of(8L, 12L, 16L)
+        );
+        assertFalse(TypeChecker.isInstanceOfString(operands));
+    }
 }
