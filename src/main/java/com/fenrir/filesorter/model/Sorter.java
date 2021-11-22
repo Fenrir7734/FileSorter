1cd3abe0e3f2792f0ad527c24af0bcb556ec205d
Fenrir7734 <hitex1999@gmail.com>
2021-11-22 13:29:17 +0100

    basic sorting

--- src/main/java/com/fenrir/filesorter/model/Sorter.java
+++ src/main/java/com/fenrir/filesorter/model/Sorter.java
@@ -1,53 +1,25 @@
 package com.fenrir.filesorter.model;
 
+import com.fenrir.filesorter.model.file.FileData;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
+
 public class Sorter {
-    /*
-    private final SortRule sortRule;
-    private final RenameRule renameRule;
-    private final List<FilterRule> filterRules;
     private final List<FileData> filesToSort;
-    private final Path targetDir;
-    private final List<? extends FilterOperatorStatement<?>> filterStatements = new ArrayList<>();
 
-    public Sorter(Path target, List<FileData> filesToSort, List<FilterRule> filterRules, SortRule sortRule, RenameRule renameRule) {
-        this.targetDir = target;
+    public Sorter(List<FileData> filesToSort) {
         this.filesToSort = filesToSort;
-        this.filterRules = filterRules;
-        this.sortRule = sortRule;
-        this.renameRule = renameRule;
     }
-    */
-    /*
+
     public void sort() throws IOException {
-        for (FileData file: filesToSort) {
-            if (!file.isDirectory()) {
-                createTargetPath(file);
-            }
-        }
-        copyFromSourceToTarget();
-    }
-
-    private void createTargetPath(FileData file) throws IOException {
-        Path dirPath = new SortRuleParser(sortRule, file).resolveRule();
-        String fileName = new RenameRuleParser(renameRule, file).resolveRule();
-        Path targetPath = targetDir.resolve(dirPath).resolve(fileName);
-        long count = countDuplicate(targetPath);
-        file.setTargetPath(targetPath, count);
-    }
-
-    private long countDuplicate(Path targetPath) {
-        return filesToSort.stream()
-                .filter(file -> !file.isDirectory())
-                .map(FileData::getTargetPath)
-                .filter(Objects::nonNull)
-                .map(Path::toString)
-                .filter(s -> s.equals(targetPath.toString()))
-                .count();
-    }
-
-    private void copyFromSourceToTarget() throws IOException {
-        for (FileData file: filesToSort) {
-            if (!file.isDirectory()) {
+        for(FileData file: filesToSort) {
+            if (file.getTargetPath() != null) {
                 copyFile(file);
             }
         }
@@ -64,6 +36,4 @@ public class Sorter {
 
         Files.copy(sourcePath, targetPath, COPY_ATTRIBUTES, NOFOLLOW_LINKS);
     }
-
-     */
 }
