9975e486682da26abe9f94c9b50ae9a5eebffceb
Fenrir7734 <hitex1999@gmail.com>
2021-11-26 12:46:10 +0100

    configuration class for holding provided by user configuration

--- src/main/java/com/fenrir/filesorter/model/Configuration.java
+++ src/main/java/com/fenrir/filesorter/model/Configuration.java
@@ -1,2 +1,54 @@
-package com.fenrir.filesorter.model;public class Configuration {
+package com.fenrir.filesorter.model;
+
+import com.fenrir.filesorter.model.rule.RuleGroup;
+import javafx.util.Pair;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Configuration {
+    private Path targetPath;
+    private final Set<Path> sourcePaths = new HashSet<>();
+    private final Map<String, RuleGroup> namedRuleGroup = new HashMap<>();
+
+    public void setTargetPath(Path targetPath) {
+        this.targetPath = targetPath;
+    }
+
+    public List<Path> addSourcePaths(List<Path> paths) {
+        return paths.stream()
+                .filter(sourcePaths::add)
+                .collect(Collectors.toList());
+    }
+
+    public List<Path> removeSourcePaths(List<Path> paths) {
+        return paths.stream()
+                .filter(sourcePaths::remove)
+                .collect(Collectors.toList());
+    }
+
+    public void addNamedRuleGroup(String name, RuleGroup group) {
+        namedRuleGroup.put(name, group);
+    }
+
+    public void removeRuleGroup(String name) {
+
+    }
+
+    public Path getTargetPath() {
+        return targetPath;
+    }
+
+    public Set<Path> getSourcePaths() {
+        return sourcePaths;
+    }
+
+    public Map<String, RuleGroup> getNamedRuleGroups() {
+        return namedRuleGroup;
+    }
+
+    public List<RuleGroup> getRuleGroups() {
+        return new ArrayList<>(namedRuleGroup.values());
+    }
 }
