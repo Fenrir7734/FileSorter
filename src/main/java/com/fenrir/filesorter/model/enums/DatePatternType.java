8c6433d11b7a9be2a77ffa479b58a04a38a50b64
Fenrir7734 <hitex1999@gmail.com>
2021-12-27 15:55:25 +0100

    name split into name and example

--- src/main/java/com/fenrir/filesorter/model/enums/DatePatternType.java
+++ src/main/java/com/fenrir/filesorter/model/enums/DatePatternType.java
@@ -1,33 +1,35 @@
 package com.fenrir.filesorter.model.enums;
 
 public enum DatePatternType {
-    YEAR("Y", "yyyy", "Year (1994)"),
-    YEAR_SHORT("y", "yy", "Year (00-99)"),
-    QUARTER_OF_YEAR("Q", "qqq", "Quarter of Year (Q1-Q4)"),
-    QUARTER_OF_YEAR_SHORT("q", "q", "Quarter of year (1-3)"),
-    QUARTER_OF_YEAR_NAME("R", "qqqq", "Quarter of year (3rd quarter)"),
-    MONTH("M", "MM", "Month (1-12)"),
-    MONTH_NAME("N", "mmmm", "Month (July)"),
-    MONT_NAME_SHORT("n", "mmm", "Month (Jul)"),
-    WEEK_OF_MONTH("W", "WW", "Week of month (01-05)"),
-    DAY_OF_YEAR("E", "DDD", "Day (001-366)"),
-    DAY_OF_MONTH("D", "dd", "Day (01-31)"),
-    DAY_OF_WEEK("F", "ee", "Day (01-07)"),
-    DAY_OF_WEEK_NAME("G", "EEEE", "Day (Friday)"),
-    DAY_OF_WEEK_NAME_SHORT("g", "EEE", "Day (Fri)"),
-    HOUR_OF_DAY("H", "HH", "Hour (00-23)"),
-    HOUR_OF_DAY_AM_PM("I", "hha", "Hour (0-12AM/PM)"),
-    MINUTE_OF_HOUR("m", "mm", "Minute (00-59)"),
-    SECOND_OF_MINUTE("s", "ss", "Second (00-59)");
+    YEAR("Y", "yyyy", "Year", "1994"),
+    YEAR_SHORT("y", "yy", "Year", "00-99"),
+    QUARTER_OF_YEAR("Q", "qqq", "Quarter of year", "Q1-Q4"),
+    QUARTER_OF_YEAR_SHORT("q", "q", "Quarter of Year", "1-3"),
+    QUARTER_OF_YEAR_NAME("R", "qqqq", "Quarter of Year", "3rd quarter"),
+    MONTH("M", "MM", "Month", "1-12"),
+    MONTH_NAME("N", "mmmm", "Month", "July"),
+    MONT_NAME_SHORT("n", "mmm", "Month", "Jul"),
+    WEEK_OF_MONTH("W", "WW", "Week of month", "01-05"),
+    DAY_OF_YEAR("E", "DDD", "Day of year", "001-366"),
+    DAY_OF_MONTH("D", "dd", "Day of month", "01-31"),
+    DAY_OF_WEEK("F", "ee", "Day of week", "01-07"),
+    DAY_OF_WEEK_NAME("G", "EEEE", "Day of week", "Friday"),
+    DAY_OF_WEEK_NAME_SHORT("g", "EEE", "Day od week", "Fri"),
+    HOUR_OF_DAY("H", "HH", "Hour", "00-23"),
+    HOUR_OF_DAY_AM_PM("I", "hha", "Hour", "0-12AM/PM"),
+    MINUTE_OF_HOUR("m", "mm", "Minute", "00-59"),
+    SECOND_OF_MINUTE("s", "ss", "Second", "00-59");
 
     private final String token;
     private final String pattern;
     private final String name;
+    private final String example;
 
-    DatePatternType(String token, String pattern, String name) {
+    DatePatternType(String token, String pattern, String name, String example) {
         this.token = token;
         this.pattern = pattern;
         this.name = name;
+        this.example = example;
     }
 
     public String getToken() {
@@ -40,6 +42,10 @@ public enum DatePatternType {
 
     public String getName() {
         return name;
+    }
+
+    public String getExample() {
+        return example;
     }
 
     public static DatePatternType getType(String token) {
--- src/test/java/com/fenrir/filesorter/model/enums/DateTokenTypeTest.java
+++ src/test/java/com/fenrir/filesorter/model/enums/DateTokenTypeTest.java
@@ -12,6 +12,24 @@ class DateTokenTypeTest {
     }
 
     @Test
+    public void getNameShouldReturnDatePatternName() {
+        DatePatternType patternType = DatePatternType.YEAR;
+        assertEquals("Year", patternType.getName());
+    }
+
+    @Test
+    public void getExampleShouldReturnDatePatternExample() {
+        DatePatternType patternType = DatePatternType.YEAR;
+        assertEquals("1994", patternType.getExample());
+    }
+
+    @Test
+    public void getTokenShouldReturnDatePatternToken() {
+        DatePatternType patternType = DatePatternType.YEAR;
+        assertEquals("Y", patternType.getToken());
+    }
+
+    @Test
     public void geTypeShouldReturnNullForInvalidToken() {
         DatePatternType patternType = DatePatternType.getType("Invalid Token");
         assertNull(patternType);
