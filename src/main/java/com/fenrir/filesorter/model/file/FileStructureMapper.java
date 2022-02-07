859263332c157350961a53e9ff2ce8ca4a1ba1ed
Fenrir7734 <hitex1999@gmail.com>
2022-02-07 15:12:47 +0100

    Files.walk change to Files.walkFileTree

--- src/main/java/com/fenrir/filesorter/model/file/FileStructureMapper.java
+++ src/main/java/com/fenrir/filesorter/model/file/FileStructureMapper.java
@@ -1,29 +1,57 @@
 package com.fenrir.filesorter.model.file;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class FileStructureMapper {
+    private static final Logger logger = LoggerFactory.getLogger(FileStructureMapper.class);
+
+    private static ArrayDeque<Path> fileHierarchy;
+
     private FileStructureMapper() {
         throw new UnsupportedOperationException();
     }
 
     public static Deque<Path> map(Path source) throws IOException {
-        Deque<Path> fileHierarchy = new ArrayDeque<>();
-        Files.walk(source)
-                .map(FileData::normalizeFilePath)
-                .filter(Objects::nonNull)
-                .forEach(fileHierarchy::push);
-        return fileHierarchy;
+        fileHierarchy = new ArrayDeque<>();
+        Files.walkFileTree(source, new MappingFileVisitor());
+        return fileHierarchy.clone();
     }
 
-    public static Deque<Path> mapDirectoriesHierarchy(Path source) throws IOException {
-        Deque<Path> directoriesHierarchy = new ArrayDeque<>();
-        Files.walk(source)
-                .filter(Files::isDirectory)
-                .forEach(directoriesHierarchy::push);
-        return directoriesHierarchy;
+    private static class MappingFileVisitor extends SimpleFileVisitor<Path> {
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            storePath(dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            storePath(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            if (Files.isDirectory(file)) {
+                logger.warn("Failed to open directory: {}", exc.getMessage());
+            } else {
+                logger.warn("Failed to visit file: {}", exc.getMessage());
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        private void storePath(Path path) {
+            path = FileData.normalizeFilePath(path);
+            if (path != null) {
+                fileHierarchy.push(path);
+            }
+        }
     }
 }
